cc44f7e2c654cdc02ef8dd4478d73d56b44ae2a8
yotam <yotam.feldman@gmail.com>
2023-01-30 17:28:46 +0200

    print also cycle energies when encoutering potential numerical instability in the forces

--- ipi/utils/exchange.py
+++ ipi/utils/exchange.py
@@ -116,6 +116,7 @@ class ExchangePotential(dobject):
             print("Numerical instability suspected", file=sys.stderr)
             print("Connection probabilities\n", connection_probs, file=sys.stderr)
             print("Potentials\n", self._V, self._V_backward, file=sys.stderr)
+            print("Cycle energies\n", self._E_from_to)
             assert False
 
         # on the last bead:
